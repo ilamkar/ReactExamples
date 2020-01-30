3eda355fd679d7040ff1ffb6eca3765ba7d49ac4
ilamkar <illum_dae@yahoo.com>
2020-01-29 21:49:28 -0600

    add event handler for goals

--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import './NewGoal.css'
+
 const b=()=>{
 
-    return <form>
+    const addGoalHandler =event=>{
+
+       event.preventDefault(); // prevent sending request to backend
+
+       const newGoal={
+            id: Math.random().toString(),
+            text: 'My New Goal'
+       };
+       console.log(newGoal);
+    };
+
+    return (
+    <form className="new-goal" onSubmit={addGoalHandler}>
         <input type="text"/>
-        <button>Add Goal</button>
+        <button type="submit">Add Goal</button>
     </form>
+    )
 }
 
  export default b;
